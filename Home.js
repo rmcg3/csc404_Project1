2cf302e01e7aa05cbffa4d697ab65c595f051e04
rmcgarry3 <47362038+rmcgarry3@users.noreply.github.com>
2020-02-08 22:11:30 -0500

    Update Home.js

--- Home.js
+++ Home.js
@@ -5,7 +5,7 @@ displayInfo = () => {
          "the letter grades (for the first 4 CSC classes) and will\n"+
          "generate a partial GPA. You will then be allowed to enter\n"+
          "a threshold GPA for approval and get a list of students whose\n"+
-         "partial GPA is greater than or equal to the threshold\n\n"
+         "partial GPA is greater than or equal to the threshold.\n\n"
 }
 console.log(displayInfo())
 
